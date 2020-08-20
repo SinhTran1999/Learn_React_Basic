80376651bae294454a0e79fe0e84a4c7bc9e13f5
Tran Nhan Sinh <trannhansinhcntt@gmail.com>
2020-08-20 21:32:36 +0700

    Reactjs Lists

--- 16.ReactLists/lists-example.jsx
+++ 16.ReactLists/lists-example.jsx
@@ -1,44 +1,44 @@
-//Employee Component
-class Employee extends React.Component{
-    //User render() function to return JSX component
-    render(){
-        return(
-            <li className="employee">
-                <div>
-                    <b>Fullname: </b>{this.props.fullName}
-                </div>  
-                <div>
-                    <b>Gender: </b>{this.props.gender}
-                </div>
-            </li>
+// Employee Component
+class Employee extends React.Component {
+    render() {
+      return (
+        <li className="employee">
+          <div>
+            <b>Full Name:</b> {this.props.fullName}
+          </div>
+          <div>
+            <b>Gender:</b> {this.props.gender}
+          </div>
+        </li>
+      );
+    }
+  }
+   
+  // EmployeeList Component
+  class EmployeeList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        employees: [
+          { empId: 1, fullName: "Trump", gender: "Male" },
+          { empId: 2, fullName: "Ivanka", gender: "Female" },
+          { empId: 3, fullName: "Kushner", gender: "Male" }
+        ]
+      };
+    }
+   
+    render() {
+      // Array of <Employee>
+      var listItems = this.state.employees.map(e => (
+        <Employee fullName={e.fullName} gender={e.gender} />
+      ));
+      return (
+          <ul className="employee-list">
+             {listItems}
+          </ul>
         );
     }
-}
-
-//EmployeeList Component
-class EmployeeList extends React.Component{
-    constructor(props){
-        super(props);
-        this.state ={
-            employees: [
-                {empId: 1, fullName: "Trump",gender: "Male"},
-                {empId: 2, fullName: "Ivanka",gender: "Female"},   
-                {empId: 3, fullName: "Kushner",gender: "Male"}
-            ]
-        };
-    }
-    render(){
-        //Array of <Employee>
-        var listItems = this.state.employees.map(e => (
-            <Employee fullName ={e.fullName} gender={e.gender}/>
-        ));
-    
-        return(
-            <ul className ="employee-list">
-                {listItems}
-            </ul>
-        );
-    }
-}
-//Render
-ReactDOM.render(<EmployeeList/>, document.getElementById("employeelist1"));
+  }
+   
+  // Render
+  ReactDOM.render(<EmployeeList />, document.getElementById("employeelist1"));
